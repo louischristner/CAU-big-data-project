99d8ce5466c0a4fa5b965d0061c86f063685e19b
momo1005 <morgan.yakhelef@hotmail.com>
2021-12-12 01:52:10 +0900

    cleaning data end

--- cleaning_data/cleaning2.py
+++ cleaning_data/cleaning2.py
@@ -26,13 +26,29 @@ lessfreq = pd.Series(' '.join(data['ign_review']).split()).value_counts()[-1000:
 #print(lessfreq)
 data['ign_review'] = data['ign_review'].apply(lambda x: " ".join(x for x in x.split() if x not in lessfreq))
 
-#spelling correction
-#data['ign_review'][:5].apply(lambda x: str(TextBlob(x).correct()))
+#spelling correction - take time
+data['ign_review'][:5].apply(lambda x: str(TextBlob(x).correct()))
 
-#Lemmatization
+#Lemmatization - TAKE SO MUCH TIME (around 25 minutes i think)
+print("debut lemmatization")
 data['ign_review'] = data['ign_review'].apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
 
 #Tokenization
-TextBlob(data['ign_review'][1]).words
+print("debut tokenization")
 
-#print(data['ign_review'])
+#methode 1
+i=0
+while i<len(data['ign_review']):
+    TextBlob(data['ign_review'][i]).words #if you want to use the data with token you need this line !
+    #print(TextBlob(data['ign_review'][i]).words)
+
+print(TextBlob(data['ign_review'][1]).words)
+
+#methode 2
+# critics_clean=[]
+# j=0
+# while j<len(data['ign_review']):
+#     critics_clean.append([i.split(" ") for i in data['ign_review'][i]])
+
+# print(critics_clean[1])
+
