2f831bdfdf1be5bf756e61b466b965c60525db67
momo1005 <morgan.yakhelef@hotmail.com>
2021-12-11 19:40:32 +0900

    [add] begining stop word
    nltk error to fix

--- cleaning_data/cleaning.py
+++ cleaning_data/cleaning.py
@@ -1,5 +1,7 @@
+from nltk.corpus import stopwords
 import csv
 import string
+
 
 file = open('databienseparer_utf8.csv', "r", encoding="utf-8")
 line = file.readlines() #get the line of the file in the tab line
@@ -15,10 +17,11 @@ for i in tab:
 # !"#$%&'()*+,-./:;<=>?@[\]^_`{}~ (we don't remove the | because it's the row separator)
 # using the terminal to do it : sed 's/,//g' databienseparer.csv > databienseparer.csv
 
-print(critics[1])
+#Removal of Stop Word (using nltk libraries)
+stop = stopwords.words('english')
 
 
-#Removal of Stop Word (using predefined libraries)
+
 #Common word removal (as the name of the game or the names of the characters) ?
 #Using librairie to spelling corrections
 #Lemmatization : convert word into its root word
